0b169c4a8f918d73597d435fef0edbca1d0bdab1
Mattéo <matteo.s@outlook.fr>
2024-10-04 10:49:11 +0200

    a

--- packaging_tutorial/biketrauma/__init__.py
+++ packaging_tutorial/biketrauma/__init__.py
@@ -1,1 +1,8 @@
 __version__ = "0.0.1"
+import biketrauma
+
+df = biketrauma.Load_db().save_as_df()
+biketrauma.plot_location(biketrauma.get_accident(df))
+from .io.Load_db import Load_db
+from .vis.plot_location import plot_location
+from .preprocess.get_accident import get_accident
